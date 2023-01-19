fba7ef26bb0a2b5d0c5d2907b4547e9e1e0df403
João Guilherme Da Rocha <joaoguibc@gmail.com>
2023-01-19 18:28:21 +0000

    Add custom fonts

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,11 +1,36 @@
-import { StatusBar } from 'expo-status-bar'
-import { Text, View } from 'react-native'
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_800ExtraBold,
+} from '@expo-google-fonts/inter'
+import { Text, View, StatusBar } from 'react-native'
+
+import { Loading } from './src/components/Loading'
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_800ExtraBold,
+  })
+
+  if (!fontsLoaded) {
+    return <Loading />
+  }
+
   return (
-    <View>
-      <Text>Hello World</Text>
-      <StatusBar style="auto" />
+    <View style={{ backgroundColor: '#09090A' }}>
+      <Text style={{ color: '#FFFFFF', fontFamily: 'Inter_800ExtraBold' }}>
+        Hello World
+      </Text>
+      <StatusBar
+        barStyle="light-content"
+        translucent
+        backgroundColor="transparent"
+      />
     </View>
   )
 }
