32c40385220f7a5929f3f099997a6a5584e76293
joaoguibc <joaoguibc@gmail.com>
2023-01-21 17:15:12 -0300

    Create dialog component

--- web/src/components/Button.tsx
+++ web/src/components/Button.tsx
@@ -1,12 +1,28 @@
-import { Plus } from 'phosphor-react'
+import { Check, Plus } from 'phosphor-react'
+import { ButtonHTMLAttributes } from 'react'
 
-export function Button() {
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  styleType?: 'primary' | 'secondary'
+}
+
+const styles = {
+  primary: 'border border-violet-500 px-6 py-4 hover:border-violet-300',
+  secondary: 'mt-6 p-4 bg-green-600 hover:bg-green-500',
+}
+
+export function Button({ styleType = 'primary', ...rest }: ButtonProps) {
   return (
     <button
-      type="button"
-      className="border border-violet-500 font-semibold rounded-lg px-6 py-4 flex items-center gap-3 hover:border-violet-300 transition-colors"
+      className={`rounded-lg flex items-center justify-center gap-3 font-semibold transition-all ${
+        styleType === 'primary' ? styles.primary : styles.secondary
+      }`}
+      {...rest}
     >
-      <Plus size={20} className="text-violet-500" />
+      {styleType === 'primary' ? (
+        <Plus size={20} className="text-violet-500" />
+      ) : (
+        <Check size={20} weight="bold" />
+      )}
       Novo hábito
     </button>
   )
---
+++ web/src/components/Dialog.tsx
@@ -0,0 +1,59 @@
+import {
+  Root,
+  Trigger,
+  Portal,
+  Overlay,
+  Content,
+  Close,
+  Title,
+} from '@radix-ui/react-dialog'
+import { ReactNode } from 'react'
+import { X } from 'phosphor-react'
+
+import { Button } from './Button'
+
+interface DialogProps {
+  children: ReactNode
+}
+
+export function Dialog({ children }: DialogProps) {
+  return (
+    <Root>
+      <Trigger>{children}</Trigger>
+
+      <Portal>
+        <Overlay className="w-screen h-screen bg-black/80 fixed inset-0" />
+
+        <Content className="absolute p-10 bg-zinc-900 rounded-2xl w-full max-w-md top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+          <Close className="absolute right-6 top-6 text-zinc-400 hover:text-zinc-200 transition-colors">
+            <X size={24} aria-label="Fechar" />
+          </Close>
+
+          <Title className="text-3xl leading-tight font-extrabold">
+            Criar hábito
+          </Title>
+
+          <form className="w-full flex flex-col mt-6">
+            <label htmlFor="title" className="font-semibold leading-tight">
+              Qual seu comprometimento?
+            </label>
+
+            <input
+              type="text"
+              id="title"
+              placeholder="ex.: exercícios, dormir bem, etc..."
+              autoFocus
+              className="p-4 rounded-lg mt-3 bg-zinc-800 text-white placeholder:text-zinc-400"
+            />
+
+            <label htmlFor="" className="font-semibold leading-tight mt-4">
+              Qual a recorrência?
+            </label>
+
+            <Button styleType="secondary" />
+          </form>
+        </Content>
+      </Portal>
+    </Root>
+  )
+}
